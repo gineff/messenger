3739abaa8360244f790d30c01435f39e864cf19e
gineff <canone@inbox.ru>
2022-09-28 13:38:59 +0300

    resserve 13:38 components.js

--- src/utils/component.js
+++ src/utils/component.js
@@ -8,7 +8,7 @@ import { useContext, nextId } from "./index";
 /* eslint-disable no-useless-escape */
 //              1           2                3         4                5
 // re =      <(Tag) (props=" props" )/> | <(Tag) (props = "props" )>(children)</Tag>
-const re = /<([A-Z][A-Za-z0-9._]+)([^>]*)\/>|<(?<tag>[A-Z][A-Za-z0-9._]+)([^>]*)>(.*?)<\/\k<tag>\s?>/;
+const re = /<([A-Z][A-Za-z0-9._]+)([^>]*)\/>|<(?<tag>[A-Z][A-Za-z0-9._]+)([^>]*)>(.*?)<\/\k<tag>\s?>|context:(\d+)/;
 
 const propsRegexp = /(\w+)\s*=\s*((?<quote>["'`])(.*?)\k<quote>|context:(\d+))|(\w+)/g;
 const components = new Map();
@@ -86,7 +86,7 @@ function isPrimitive(element) {
   return Object(element) !== element;
 }
 
-function wrapToArray(element) {
+function wrapWithArray(element) {
   return Array.isArray(element) ? element : [element];
 }
 
@@ -94,13 +94,13 @@ function createContextElement(context) {
   let nodes;
   const contextValue = getContext(context);
   if (isComponentInstance(contextValue)) {
-    const comps = wrapToArray(contextValue);
+    const comps = wrapWithArray(contextValue);
     nodes = comps.reduce((arr, component) => {
       arr.push(...component.render());
       return arr;
     }, []);
   } else {
-    nodes = wrapToArray(document.createTextNode(contextValue));
+    nodes = wrapWithArray(document.createTextNode(contextValue));
   }
   return nodes;
 }
@@ -110,12 +110,17 @@ function parseNestedComponents(block) {
   const collection = {};
 
   while ((match = block.match(re))) {
-    const [found, singleTag, singleTagProps, pairedTag, pairedTagProps, children] = match;
-    const props = parseProps(singleTagProps || pairedTagProps);
+    const [found, singleTag, singleTagProps, pairedTag, pairedTagProps, children, context] = match;
     const id = nextId();
-
     block = block.replace(found, `<component id="${id}">${children || ""}</component>`);
-    collection[id] = new (components.get(singleTag || pairedTag))({ ...props });
+
+    if (context) {
+      collection[id] = getContext(context);
+      // компонент или массив компонентов
+    } else {
+      const props = parseProps(singleTagProps || pairedTagProps);
+      collection[id] = new (components.get(singleTag || pairedTag))({ ...props });
+    }
   }
 
   return [block, collection];
@@ -162,13 +167,14 @@ export default class Component {
     // this.container.forEach((element) => addEventHandler(element, this.state));
 
     // дублирование на компоненте и див?
-    return this;
+    return this.fragment;
   }
 
   _render() {
     const block = this._compile(this.template).replace(/\n|\s{2}/g, "");
     const [htmlTree, nestedComponents] = parseNestedComponents(block);
 
+    console.log(nestedComponents);
     const fragment = createElementFromString(htmlTree);
 
     Object.entries(nestedComponents).forEach(([id, component]) => {
@@ -177,8 +183,19 @@ export default class Component {
       if (fragmentElement.childNodes.length > 0) {
         component.state = { ...component.state, children: fragmentElement.childNodes };
       }
+      const componentFragment = wrapWithArray(component).reduce((container, comp) => {
+        try {
+          container.append(comp.render());
+        } catch (e) {
+          console.error(e, comp);
+        }
+        return container;
+      }, new DocumentFragment());
 
-      console.log(component.render());
+      // createContextElement
+
+      fragmentElement.replaceWith(componentFragment);
+      // console.log(component.render());
 
       // component.render()
     });

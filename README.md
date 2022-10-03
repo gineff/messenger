4f4a6014893e9d8902c5fd55cf4b331c0d90b6fd
gineff <canone@inbox.ru>
2022-10-04 01:56:49 +0300

    1-st sprint pull request

---
+++ README.md
@@ -0,0 +1,16 @@
+## Описание проекта
+
+Мессенджер
+
+## Команды установки
+
+`npm i` - установка зависимостей
+`npm run dev` - запуск приложения в режиме разработки
+`npm run build` - сборка проекта
+`nmp start` - запуск express-сервера
+
+## Макет
+
+https://www.figma.com/file/jF5fFFzgGOxQeB4CmKWTiE/Chat_external_link
+
+## Приложение на netlify
--- index.js
+++ index.js
@@ -5,15 +5,14 @@ const app = express();
 
 // app.use("/", express.static(path.join(__dirname, "/dist")));
 
+app.get(/\.[a-z0-9]+/i, (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", req.originalUrl));
+});
+
 app.use("/images", express.static(path.join(__dirname, "/dist/images")));
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "/dist/index.html"));
 });
-
-app.get(/\.[a-z0-9]+/i, (req, res) => {
-  res.sendFile(path.join(__dirname, "/dist/index.html"));
-});
-
 
 app.listen(3000, () => console.log("Server http://localhost:3000"));
